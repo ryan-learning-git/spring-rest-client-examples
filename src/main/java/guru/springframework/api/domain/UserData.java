4878c414e08eb9cdc4ba800764d33c035f263f3b
Ryan Williams <ryan.williams@projectpeople.com>
2017-11-14 11:30:26 +0000

    Completed Lesson 266

---
+++ src/main/java/guru/springframework/api/domain/UserData.java
@@ -0,0 +1,14 @@
+package guru.springframework.api.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class UserData {
+
+    private List<User> data;
+
+}
